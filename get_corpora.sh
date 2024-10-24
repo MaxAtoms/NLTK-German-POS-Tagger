58c37d4ccc6e4d7b015d708f34cf82f344b3a2fe
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2024-10-24 15:32:26 +0000

    Add out-of-domain corpora

--- get_corpora.sh
+++ get_corpora.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# TIGER Korpus
 wget https://www.ims.uni-stuttgart.de/documents/ressourcen/korpora/tiger-corpus/download/tigercorpus-2.2.conll09.tar.gz --no-verbose -P corpora/
 tar -xzvf corpora/tigercorpus-2.2.conll09.tar.gz -C corpora/
+
+# Gold Standard from "Evaluating Off-the-Shelf NLP Tools for German" (Ortmann et al., 2019)
+wget https://raw.githubusercontent.com/rubcompling/konvens2019/refs/heads/master/data/gold/balanced/annotations/novelette.conll --no-verbose -P corpora/
+wget https://raw.githubusercontent.com/rubcompling/konvens2019/refs/heads/master/data/gold/balanced/annotations/opensubtitles.conll --no-verbose -P corpora/
+wget https://raw.githubusercontent.com/rubcompling/konvens2019/refs/heads/master/data/gold/balanced/annotations/sermononline.conll --no-verbose -P corpora/
+wget https://raw.githubusercontent.com/rubcompling/konvens2019/refs/heads/master/data/gold/balanced/annotations/ted.conll --no-verbose -P corpora/
+wget https://raw.githubusercontent.com/rubcompling/konvens2019/refs/heads/master/data/gold/balanced/annotations/wikipedia.conll --no-verbose -P corpora/
