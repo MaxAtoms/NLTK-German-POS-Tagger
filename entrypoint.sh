67f255ed3b8d498b2ea4f1ff7c7984520ee0a84f
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2024-10-24 15:01:34 +0000

    Add GitHub Action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+# Work-around for GitHub action issue :(
+pip install --user pipenv
+
+echo $(who)
+echo $(id)
+echo $(pip show pipenv)
 
 pipenv install
 ./get_corpora.sh
--- train_eval.sh
+++ train_eval.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+set -e
+
+print_dashes() {
+  printf -- '-%.s' {1..58}
+  echo
+}
+
+print_dashes
 pipenv run python ./german_tagger.py --data 1 --iter 1 --percentage 1
+print_dashes
 pipenv run python ./german_tagger.py --data 2 --iter 1 --percentage 80
+print_dashes
 pipenv run python ./german_tagger.py --data 3 --iter 1 --percentage 90
 
+print_dashes
+echo "Result for a test split of 1%/99%"
 pipenv run python ./evaluator.py --data 1 --model 1
+print_dashes
+echo "Result for a test split of 80%/20%"
 pipenv run python ./evaluator.py --data 2 --model 1
+print_dashes
+echo "Result for a test split of 90%/10%"
 pipenv run python ./evaluator.py --data 3 --model 1
 
